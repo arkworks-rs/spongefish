617c9be7adf27273b6e9fbb9a3234acb93c0caba
Michele Orrù <m@orru.net>
2025-10-27 12:54:33 +0100

    add p3_poseidon2 implementation as experiment with poseidon2 sponge.

--- spongefish/src/drivers/mod.rs
+++ spongefish/src/drivers/mod.rs
@@ -1,36 +1,28 @@
 //! Bindings to some popular libraries using zero-knowledge.
 
-// Arkworks field implementations
+// arkworks-rs
+#[cfg(feature = "ark-ec")]
+pub mod ark_ec_impl;
 #[cfg(feature = "ark-ff")]
 pub mod ark_ff_impl;
 
-// Arkworks elliptic curve implementations
-#[cfg(feature = "ark-ec")]
-pub mod ark_ec_impl;
-
-// Module for BLS12-381 support
+// zkcrypto
 #[cfg(feature = "bls12_381")]
 pub mod bls12_381_impl;
-
-// Module for curve25519-dalek support
 #[cfg(feature = "curve25519-dalek")]
 pub mod curve25519_dalek_impl;
-
-// Module for secp256k1 support (k256)
 #[cfg(feature = "k256")]
 pub mod secp256k1_impl;
 
-// Plonky3 BabyBear field
+// Plonky3
 #[cfg(feature = "p3-baby-bear")]
 pub mod p3_baby_bear;
-
-// Plonky3 Mersenne31 field
-#[cfg(feature = "p3-mersenne-31")]
-pub mod p3_mersenne31;
-
-// Plonky3 KoalaBear field
 #[cfg(feature = "p3-koala-bear")]
 pub mod p3_koala_bear;
+#[cfg(feature = "p3-mersenne-31")]
+pub mod p3_mersenne31;
+#[cfg(feature = "p3-poseidon2")]
+pub mod p3_poseidon2;
 
 // Buffer of 512-bytes, useful for decoding 256-bit scalars.
 #[repr(C)]
