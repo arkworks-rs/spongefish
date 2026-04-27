2c5e927794df7bee27be3fc58252fc3d26b6ac60
Michele Orru <m@orru.net>
2026-04-26 23:42:03 -0400

    chore(circuit): define upper-limit of wire vars.
    The current circuit API is very primitive and basic, and does not impose any limit on the wire value,
    nor it checks for overflow, incorrect usages of zip, etc.
    Additionally, downstream of this crate, implementations will most likely need to convert the wire index into a field elements.
    
    This commit tries to improve the status quo with an upper-limit of wire vars of 2^30 (so it can just fit a group element for most finite fields used in zkps). Along the way, we improve the API for setting public vars

--- circuit/src/allocator.rs
+++ circuit/src/allocator.rs
@@ -1,14 +1,43 @@
 //! Defines the allocator and wires to be used for computing the key-derivation steps.
 
 use alloc::{sync::Arc, vec::Vec};
+use core::borrow::Borrow;
 
+use hashbrown::HashMap;
+use itertools::Itertools;
 use spin::RwLock;
 use spongefish::Unit;
 
 /// A symbolic wire over which we perform out computation.
-/// Wraps over a [`usize`]
-#[derive(Clone, Copy, Default, Hash, PartialEq, Eq, Unit)]
-pub struct FieldVar(pub usize);
+#[derive(Clone, Copy, Default, Hash, PartialEq, Eq)]
+pub struct FieldVar(usize);
+
+impl FieldVar {
+    /// Maximum number of variables supported by the circuit allocator.
+    pub const MAX_COUNT: usize = 1 << 30;
+    /// The distinguished zero variable.
+    pub const ZERO: Self = Self(0);
+
+    /// Return the variable index.
+    #[must_use]
+    pub const fn index(self) -> usize {
+        self.0
+    }
+
+    /// Construct a variable from an index when it is within the supported range.
+    #[must_use]
+    pub const fn try_from_index(index: usize) -> Option<Self> {
+        if index < Self::MAX_COUNT {
+            Some(Self(index))
+        } else {
+            None
+        }
+    }
+}
+
+impl Unit for FieldVar {
+    const ZERO: Self = Self::ZERO;
+}
 
 impl core::fmt::Debug for FieldVar {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
@@ -27,7 +56,7 @@ pub struct VarAllocator<T> {
 
 struct AllocatorState<T> {
     vars_count: usize,
-    public_values: Vec<(FieldVar, T)>,
+    public_values: HashMap<FieldVar, T>,
 }
 
 impl<T: Clone + Unit> Default for VarAllocator<T> {
@@ -40,10 +69,12 @@ impl<T: Clone + Unit> VarAllocator<T> {
     #[must_use]
     pub fn new() -> Self {
         let zero_var = FieldVar::ZERO;
+        let mut public_values = HashMap::new();
+        public_values.insert(zero_var, T::ZERO);
         Self {
             state: Arc::new(RwLock::new(AllocatorState {
                 vars_count: 1,
-                public_values: Vec::from([(zero_var, T::ZERO)]),
+                public_values,
             })),
         }
     }
@@ -51,6 +82,11 @@ impl<T: Clone + Unit> VarAllocator<T> {
     #[must_use]
     pub fn new_field_var(&self) -> FieldVar {
         let mut state = self.state.write();
+        assert!(
+            state.vars_count < FieldVar::MAX_COUNT,
+            "variable count exceeds supported maximum {}",
+            FieldVar::MAX_COUNT,
+        );
         let var = FieldVar(state.vars_count);
         state.vars_count += 1;
         var
@@ -67,6 +103,18 @@ impl<T: Clone + Unit> VarAllocator<T> {
 
     #[must_use]
     pub fn allocate_vars_vec(&self, count: usize) -> Vec<FieldVar> {
+        {
+            let state = self.state.read();
+            let new_count = state
+                .vars_count
+                .checked_add(count)
+                .expect("variable count overflow");
+            assert!(
+                new_count <= FieldVar::MAX_COUNT,
+                "variable count exceeds supported maximum {}",
+                FieldVar::MAX_COUNT,
+            );
+        }
         (0..count).map(|_| self.new_field_var()).collect()
     }
 
@@ -87,27 +135,51 @@ impl<T: Clone + Unit> VarAllocator<T> {
         self.state.read().vars_count
     }
 
-    pub fn set_public_var(&self, val: FieldVar, var: T) {
-        self.state.write().public_values.push((val, var));
+    #[must_use]
+    pub fn is_allocated(&self, var: FieldVar) -> bool {
+        var.index() < self.vars_count()
     }
 
+    /// Assigns the wire variable `var` to `val`.
+    ///
+    /// If the wire was already present, it is over-written.
+    pub fn set_public_var(&self, var: FieldVar, val: T) {
+        self.state.write().public_values.insert(var, val);
+    }
+
+    /// Sets a list of public variables.
+    ///
+    /// Takes as input two iterators (for wires and values respectively),
+    /// and sets each of them to public values.
+    ///
+    /// # Panics
+    ///
+    /// If the iterators have different length, this function will panic.
     pub fn set_public_vars<Val, Var>(
         &self,
         vars: impl IntoIterator<Item = Var>,
         vals: impl IntoIterator<Item = Val>,
     ) where
-        Var: core::borrow::Borrow<FieldVar>,
-        Val: core::borrow::Borrow<T>,
+        Var: Borrow<FieldVar>,
+        Val: Borrow<T>,
     {
         self.state.write().public_values.extend(
             vars.into_iter()
-                .zip(vals)
+                .zip_eq(vals)
                 .map(|(var, val)| (*var.borrow(), val.borrow().clone())),
         );
     }
 
     #[must_use]
     pub fn public_vars(&self) -> Vec<(FieldVar, T)> {
-        self.state.read().public_values.clone()
+        let mut public_values = self
+            .state
+            .read()
+            .public_values
+            .iter()
+            .map(|(var, val)| (*var, val.clone()))
+            .collect::<Vec<_>>();
+        public_values.sort_unstable_by_key(|(var, _)| var.index());
+        public_values
     }
 }
--- circuit/tests/builder.rs
+++ circuit/tests/builder.rs
@@ -1,8 +1,11 @@
 use p3_baby_bear::BabyBear;
 use spongefish::{DuplexSponge, DuplexSpongeInterface, Permutation};
-use spongefish_circuit::permutation::{
-    LinearEquation, PermutationInstanceBuilder, PermutationWitnessBuilder,
+use spongefish_circuit::{
+    allocator::FieldVar,
+    permutation::{LinearEquation, PermutationInstanceBuilder, PermutationWitnessBuilder},
 };
+
+type TestInstanceBuilder = PermutationInstanceBuilder<BabyBear, 16>;
 
 #[derive(Clone, Default)]
 struct DummyPermutation;
@@ -15,12 +18,31 @@ impl Permutation<16> for DummyPermutation {
     }
 }
 
+fn instance_builder() -> TestInstanceBuilder {
+    PermutationInstanceBuilder::new()
+}
+
+fn assert_panics_with(expected: &str, f: impl FnOnce()) {
+    let panic = std::panic::catch_unwind(std::panic::AssertUnwindSafe(f))
+        .expect_err("expected closure to panic");
+    let message = panic
+        .downcast_ref::<String>()
+        .map(String::as_str)
+        .or_else(|| panic.downcast_ref::<&str>().copied())
+        .unwrap_or("<non-string panic>");
+
+    assert!(
+        message.contains(expected),
+        "panic message {message:?} did not contain {expected:?}",
+    );
+}
+
 #[test]
 pub fn test_xof() {
     // Create a new dummy permutation.
     // The permutation contains internally a "FieldVar" allocator, which is simply a `usize`
     // representing a field variable.
-    let inst_builder = PermutationInstanceBuilder::<BabyBear, 16>::new();
+    let inst_builder = instance_builder();
 
     // You can access the allocator with .allocator()..
     // .. and allocate new variables (in this case 13) that are private ..
@@ -47,7 +69,7 @@ pub fn test_xof() {
     // Let's assume the output is public (that's the case in Fiat-Shamir or in encryption)
     inst_builder
         .allocator()
-        .set_public_vars(&xof_output, [BabyBear::new(42); 3]);
+        .set_public_vars(&xof_output, [BabyBear::new(42); 4]);
 
     // Since rate = 8 and |public + secret| = 16
     // we have invoked the permutation function twice.
@@ -64,8 +86,10 @@ pub fn test_xof() {
 
 #[test]
 pub fn test_linear_equations() {
-    let inst_builder = PermutationInstanceBuilder::<BabyBear, 16>::new();
-    let [a, b, c] = inst_builder.allocator().allocate_vars();
+    let inst_builder = instance_builder();
+    let vars = inst_builder.allocator().allocate_vars::<16>();
+    let [a, b, c] = [vars[0], vars[1], vars[2]];
+    inst_builder.add_permutation(vars, vars);
     inst_builder.add_equation(LinearEquation::new(
         [
             (BabyBear::new(1), a),
@@ -113,4 +137,98 @@ pub fn test_witness_linear_equations() {
         (BabyBear::new(6), BabyBear::new(8))
     );
     assert_eq!(equations.as_ref()[0].image, BabyBear::new(9));
+}
+
+#[test]
+pub fn field_var_indices_are_bounded() {
+    assert_eq!(
+        FieldVar::try_from_index(FieldVar::MAX_COUNT - 1)
+            .expect("last valid variable")
+            .index(),
+        FieldVar::MAX_COUNT - 1
+    );
+    assert!(FieldVar::try_from_index(FieldVar::MAX_COUNT).is_none());
+}
+
+#[test]
+pub fn public_vars_are_returned_by_variable_index() {
+    let inst_builder = instance_builder();
+    let [first, second] = inst_builder.allocator().allocate_vars();
+
+    inst_builder
+        .allocator()
+        .set_public_var(second, BabyBear::new(2));
+    inst_builder
+        .allocator()
+        .set_public_var(first, BabyBear::new(1));
+
+    assert_eq!(
+        inst_builder.allocator().public_vars(),
+        vec![
+            (FieldVar::ZERO, BabyBear::new(0)),
+            (first, BabyBear::new(1)),
+            (second, BabyBear::new(2)),
+        ]
+    );
+}
+
+#[test]
+pub fn linear_equation_terms_must_be_bound_unless_zero() {
+    let inst_builder = instance_builder();
+    let bound_vars = inst_builder.allocator().allocate_vars::<16>();
+    let [unbound_var] = inst_builder.allocator().allocate_vars();
+    inst_builder.add_permutation(bound_vars, bound_vars);
+
+    assert_panics_with(
+        "nonzero linear terms must reference a permutation input or output variable",
+        || {
+            inst_builder.add_equation(LinearEquation::new(
+                [(BabyBear::new(1), unbound_var)],
+                BabyBear::new(0),
+            ));
+        },
+    );
+
+    inst_builder.add_equation(LinearEquation::new(
+        [(BabyBear::new(0), unbound_var)],
+        BabyBear::new(0),
+    ));
+
+    assert_eq!(inst_builder.linear_constraints().as_ref().len(), 1);
+}
+
+#[test]
+pub fn allocate_vars_vec_overflow_does_not_mutate_allocator() {
+    let inst_builder = instance_builder();
+    let allocator = inst_builder.allocator();
+    let vars_count = allocator.vars_count();
+
+    assert_panics_with("variable count overflow", || {
+        let _ = allocator.allocate_vars_vec(usize::MAX);
+    });
+
+    assert_eq!(allocator.vars_count(), vars_count);
+}
+
+#[test]
+pub fn snapshots_are_immutable_after_builder_mutation() {
+    let inst_builder = instance_builder();
+    let first_vars = inst_builder.allocator().allocate_vars::<16>();
+    let _ = inst_builder.allocate_permutation(&first_vars);
+    let instance_snapshot = inst_builder.snapshot();
+
+    let second_vars = inst_builder.allocator().allocate_vars::<16>();
+    let _ = inst_builder.allocate_permutation(&second_vars);
+
+    assert_eq!(instance_snapshot.constraints().as_ref().len(), 1);
+    assert_eq!(inst_builder.constraints().as_ref().len(), 2);
+
+    let witness = PermutationWitnessBuilder::<DummyPermutation, 16>::new(DummyPermutation);
+    let input = [BabyBear::new(1); 16];
+    let _ = witness.allocate_permutation(&input);
+    let witness_snapshot = witness.snapshot();
+    let _ = witness.allocate_permutation(&input);
+
+    assert_eq!(witness_snapshot.trace().as_ref().len(), 1);
+    assert_eq!(witness.trace().as_ref().len(), 2);
 }
