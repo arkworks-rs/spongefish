da02bad0672b2269d6f37d96435b2a317557f49b
Michele Orru <m@orru.net>
2026-06-03 13:48:20 -0700

    update ascon and check for endianness

--- spongefish/src/instantiations/permutations.rs
+++ spongefish/src/instantiations/permutations.rs
@@ -3,20 +3,41 @@ pub use ascon::Ascon12;
 #[cfg(feature = "keccak")]
 pub use keccak::KeccakF1600;
 
+#[cfg(any(feature = "ascon", feature = "keccak"))]
+const _: () = assert!(cfg!(target_endian = "little"));
+
 #[cfg(feature = "ascon")]
 mod ascon {
+    use crate::duplex_sponge::Permutation;
+    use core::ptr;
 
     #[derive(Clone, Debug, Default)]
     pub struct Ascon12;
-    use crate::duplex_sponge::Permutation;
 
     impl Permutation<40> for Ascon12 {
         type U = u8;
 
         fn permute(&self, state: &[u8; 40]) -> [u8; 40] {
-            let mut state = ascon::State::from(state);
-            state.permute_12();
-            state.as_bytes()
+            let mut words = [0u64; 5];
+            unsafe {
+                ptr::copy_nonoverlapping(
+                    state.as_ptr(),
+                    words.as_mut_ptr().cast::<u8>(),
+                    state.len(),
+                );
+            }
+
+            ascon::permute12(&mut words);
+
+            let mut new_state = [0u8; 40];
+            unsafe {
+                ptr::copy_nonoverlapping(
+                    words.as_ptr().cast::<u8>(),
+                    new_state.as_mut_ptr(),
+                    new_state.len(),
+                );
+            }
+            new_state
         }
     }
 }
@@ -31,10 +52,6 @@ mod keccak {
     /// or equivalently 200 bytes in little-endian order.
     #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
     pub struct KeccakF1600;
-
-    /// Make sure that we're compiling in a platform where
-    /// the use of transmute for keccak evaluations is OK.
-    const _: () = assert!(core::mem::size_of::<u64>() == 8 * core::mem::size_of::<u8>());
 
     impl Permutation<{ 136 + 64 }> for KeccakF1600 {
         type U = u8;
@@ -54,7 +71,9 @@ mod keccak {
                     state.len(),
                 );
             }
+
             keccak::f1600(&mut words);
+
             unsafe {
                 ptr::copy_nonoverlapping(
                     words.as_ptr().cast::<u8>(),
